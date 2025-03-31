ba65be888a20ef867af571226f486d6d3b6d0bfa
Kripa Rathod <kriparathod@HARSHS-Air-3.bbrouter>
2025-03-31 21:11:37 +0530

    op

--- restaurant-frontend/src/index.js
+++ restaurant-frontend/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'; // 👈 ADD THIS
-
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const basename = process.env.NODE_ENV === 'production' ? '/Madras_Meals' : '/';
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* 👈 WRAP App */}
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
